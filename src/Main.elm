1e7dfc8e3e3d5f595cb2128e1a23564e5e8567c7
Manuel Ospina <manuel@anect.jp>
2019-08-05 16:31:09 +0900

    Rest TA original status before returning schedule

--- src/Main.elm
+++ src/Main.elm
@@ -38,7 +38,18 @@ type alias TA =
 
 
 
--- interp. the TA's name, number of slots they can work, and slots they're available for
+{-
+   -- interp. the TA's name, number of slots they can work, and slots they're available for
+   fnForTa : TA -> ...
+   fnForTa ta = ... ta.name ta.max (fnForLos ta.avail)
+
+   fnForLot : List TA -> ...
+   fnForLot ta =
+     case ta of
+       [] -> []
+       first :: rest -> ... (fnForTa first)
+                            (fnForLot rest)
+-}
 
 
 type alias Assignment =
@@ -61,9 +72,193 @@ type alias Schedule =
 
 scheduleTas : List TA -> List Slot -> Maybe Schedule
 scheduleTas tas slots =
-    Just []
+    let
+        fnForS tas0 slots0 schedule =
+            case slots0 of
+                [] ->
+                    Just schedule
+
+                first :: rest ->
+                    fnForLos tas0 rest schedule (nextAssignments first tas0)
+
+        fnForLos tas1 slots1 schedule1 assignments =
+            case assignments of
+                [] ->
+                    Nothing
+
+                first :: rest ->
+                    let
+                        try =
+                            fnForS (reduceTasMax tas1 first) slots1 (first :: schedule1)
+                    in
+                    case try of
+                        Just schedule ->
+                            Just (resetTas schedule)
+
+                        _ ->
+                            fnForLos tas1 slots1 schedule1 rest
+
+        nextAssignments : Slot -> List TA -> List Assignment
+        nextAssignments slot2 tas2 =
+            let
+                fnForTa ta =
+                    List.member slot2 ta.avail
+
+                fnForLot lot acc =
+                    case lot of
+                        [] ->
+                            acc
+
+                        first :: rest ->
+                            if fnForTa first then
+                                fnForLot rest (Assignment first slot2 :: acc)
+
+                            else
+                                fnForLot rest acc
+            in
+            fnForLot tas2 []
+
+        reduceTasMax : List TA -> Assignment -> List TA
+        reduceTasMax tas3 assignment =
+            let
+                fnForTa ta =
+                    if assignment.ta == ta then
+                        { ta | max = ta.max - 1 }
+
+                    else
+                        ta
+            in
+            List.map fnForTa tas3 |> List.filter (\ta -> ta.max > 0)
+
+        resetTas : Schedule -> Schedule
+        resetTas schedule =
+            let
+                replaceTa assignment =
+                    { assignment | ta = getTa assignment.ta tas }
+
+                getTa ta tas4 =
+                    case tas4 of
+                        [] ->
+                            ta
+
+                        first :: rest ->
+                            if first.name == ta.name then
+                                first
+
+                            else
+                                getTa ta rest
+            in
+            List.map replaceTa schedule
+    in
+    fnForS tas slots []
 
 
 
+{-
+   scheduleTas : List TA -> List Slot -> Maybe Schedule
+   scheduleTas tas slots =
+       case slots of
+           [] ->
+               Just []                      -- find solution, return schedule
+
+           firstSlot :: restSlot ->         -- go into each slot and generate possible schedule
+               case tas of
+                   [] ->
+                       Nothing              -- invalid schedule
+
+                   firstTas :: restTas ->
+                       Nothing
+
+-}
 -- stub
 -- produce a Just schedule given TAs and Slots; Nothing if impossible
+-- Two one of data.
+-- backtracking search (to check valid options (like on slot 3 4)
+-- Generative to create the Schedule
+{-
+         CROSS PRODUCT OF TYPE COMMENTS TABLE
+
+
+         +----------------------------------------------+
+         | tas \ slots | []      | List Slot            |
+         |----------------------------------------------|
+         | []          |         | Nothing              |
+         |-------------| Just [] |----------------------|
+         | List TA     |         | Schedule             |
+         +----------------------------------------------+
+
+   scheduleTas
+    [ TA "Soba" 2 [ 1, 3 ]
+    , TA "Udon" 1 [ 3, 4 ]
+    , TA "Ramen" 1 [ 2 ]
+    ]
+    [ 1, 2, 3, 4 ]
+
+
+   Just ([Assignment "Soba" 1])
+   ScheduleTas
+    [ TA "Soba" 1 [ 1, 3 ]
+    , TA "Udon" 1 [ 3, 4 ]
+    , TA "Ramen" 1 [ 2 ]
+    ]
+    [ 2, 3, 4 ]
+
+   Just ([ Assignment "Soba" 1
+        , Assignment "Ramen" 2
+        ])
+   ScheduleTas
+    [ TA "Soba" 1 [ 1, 3 ]
+    , TA "Udon" 1 [ 3, 4 ]
+    , TA "Ramen" 0 [ 2 ]
+    ]
+    [ 3, 4 ]
+
+   -- Branch 1: --
+   Just ([ Assignment "Soba" 1
+        , Assignment "Ramen" 2
+        , Assignment "Soba" 3
+        ])
+   ScheduleTas
+    [ TA "Soba" 0 [ 1, 3 ]
+    , TA "Udon" 1 [ 3, 4 ]
+    , TA "Ramen" 0 [ 2 ]
+    ]
+    [ 4 ]
+
+   Just ([ Assignment "Soba" 1
+        , Assignment "Ramen" 2
+        , Assignment "Soba" 3
+        , Assignment "Udon" 4
+        ])
+   ScheduleTas
+    [ TA "Soba" 0 [ 1, 3 ]
+    , TA "Udon" 0 [ 3, 4 ]
+    , TA "Ramen" 0 [ 2 ]
+    ]
+    [ ]
+
+   -- Branch 2: --
+
+   Just ([ Assignment "Soba" 1
+        , Assignment "Ramen" 2
+        , Assignment "Udon" 3
+        ])
+   ScheduleTas
+    [ TA "Soba" 1 [ 1, 3 ]
+    , TA "Udon" 0 [ 3, 4 ]
+    , TA "Ramen" 0 [ 2 ]
+    ]
+    [ 4 ]
+
+   Just ([ Assignment "Soba" 1
+        , Assignment "Ramen" 2
+        , Assignment "Udon" 3
+        , Nothing
+        ])
+   ScheduleTas
+    [ TA "Soba" 1 [ 1, 3 ]
+    , TA "Udon" 0 [ 3, 4 ]
+    , TA "Ramen" 0 [ 2 ]
+    ]
+    []
+-}
